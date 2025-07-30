c97a70c6229fc73761f98a4d43455c0e48f957b4
Ian Boyes <igboyes@gmail.com>
2025-07-30 10:22:52 -0700

    fix: send pocketbase requests to /api

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     port: Number(process.env.PORT) || 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "http://localhost:8080/api",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
