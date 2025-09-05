50aade50c6875effd37eebfbbd9047d980945cc2
Reece Hoffmann <59776400+ReeceHoffmann@users.noreply.github.com>
2025-09-05 14:02:59 -0700

    fix: remove unused dashboard

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,9 @@
-import { Header } from "@/components/ui/header.tsx";
-import { createFileRoute } from "@tanstack/react-router";
-import "../App.css";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-  component: App,
+  beforeLoad: () => {
+    throw redirect({
+      to: "/fastqs",
+    });
+  },
 });
-
-function App() {
-  return (
-    <>
-      <Header title="Dashboard" />
-
-      <section>
-        <h2 className="font-medium text-xl mb-2">Issues</h2>
-        <p className="text-gray-500 text-sm" data-slot="issues">
-          Resolve before the data can be used for building models
-        </p>
-      </section>
-    </>
-  );
-}
